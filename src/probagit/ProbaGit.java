14a8cf108e50a676eea4233a2a7cede188dc80a0
Your Name <you@example.com>
2021-09-13 16:24:37 +0300

    Вторая часть

--- src/probagit/ProbaGit.java
+++ src/probagit/ProbaGit.java
@@ -16,8 +16,7 @@ public class ProbaGit {
      */
     public static void main(String[] args) {
         System.out.println("Начало проекта");
-        System.out.println("продолжение");
-        System.out.println("третья версия программы");
+        System.out.println("Вторая часть");
     }
     
 }
